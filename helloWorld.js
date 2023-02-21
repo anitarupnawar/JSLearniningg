834234cfa5a600e2223d352bb9281ee1b14abd1d
anitarupnawar <anitarupnawar@gmail.com>
2023-02-21 23:54:43 +0530

    I have added some details in JS 1st  program
    I have added some details in JS 1st  program

--- helloWorld.js
+++ helloWorld.js
@@ -1,2 +1,24 @@
-
+//Personal Details
 console.log ("Anita Rupnawar");
+console.log(31);
+console.log("Pune");
+
+var fullName = "Anita Rupnawar";
+console.log(fullName);
+console.log(fullName);
+
+var city; //Variable Declaration
+console.log(city);
+city="Pune"; // Variable intilization
+console.log(city);
+city="Surat"; //Variable update
+console.log(city);
+
+var age="31" // Variable Declaration and Variable intilization in same line
+age ="32";
+age ="33";
+console.log("age");
+
+var university; //Variable Declare
+university= "SPU"; // Variable intilization
+console.log("SPU");
