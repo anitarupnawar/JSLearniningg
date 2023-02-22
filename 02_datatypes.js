f5fa880c077dea7de23340a27e03c3ae1e1b8298
anitarupnawar <anitarupnawar@gmail.com>
2023-02-22 17:41:00 +0530

    I have updated 01_variables.js
    I have updated 01_variables.js

---
+++ 02_datatypes.js
@@ -0,0 +1,40 @@
+var myName = 'Anita Rupnawar';
+var fatherName = 'Mukinda Rupnawar';
+var age =31;
+var weight= 80;
+
+var isMarried = true;
+var isLazy = false;
+
+var typeofmyName =typeof myName;
+console.log("typeof variable myName: ", typeofmyName);
+console.log("typeof variable fatherName:" , typeof fatherName);
+console.log("typeof variable age:" , typeof age);
+console.log("typeof variable weight:" , typeof weight);
+console.log("typeof variable isMarried:" , typeof isMarried);
+console.log("typeof variable isLazy:" , typeof isLazy);
+
+var graduation;
+console.log("value of graduation variable is: " , graduation);
+console.log("typeof variable graduation:" , typeof graduation);
+
+var city = null;
+console.log("value of city variable is: " , city);
+console.log("typeof variable city:" , typeof city);
+
+age = "32";
+console.log(typeof age);
+
+var bankName;
+
+console.log("HDFC");
+
+console.log("swapping variables");
+
+var num1= 100;
+var num2= 200;
+
+var temp= num1;
+num1=num2;
+num2=temp;
+console.log(num1,num2);
