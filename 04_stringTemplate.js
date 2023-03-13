eef80503aa9024034475ed6c5e7267c04021cd5d
anitarupnawar <anitarupnawar@gmail.com>
2023-03-13 22:58:40 +0530

    I have update stringTemplate.js
    I have update stringTemplate.js

--- 04_stringTemplate.js
+++ 04_stringTemplate.js
@@ -1,2 +1,10 @@
 
-console.log(`You are "champ" man...`);
+console.log(`You are "champ" man...`); 
+
+var firstName = "Anita";
+var lastName = " Rupnawar";
+var city = "Pune";
+
+console.log("My First Name : " , firstName, "lastName", lastName, "City" , city);
+
+console.log(`My First Name ${firstName} last Name ${lastName} city ${city}`);
