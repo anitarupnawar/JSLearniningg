5d2bf2dc6354dfe062c38450b167c10d43a191b6
anitarupnawar <anitarupnawar@gmail.com>
2023-03-14 00:30:25 +0530

    Operators.js
    Operators.js

---
+++ 05_Operators.js
@@ -0,0 +1,64 @@
+
+console.log(`===Arithmetic Oprataors===`);
+var num1 = 10;
+var num2 = 20;
+ var result= num1 + num2; //Addition
+ console.log(`Addition is ${result}`);
+
+ var num1 = 40;
+ var num2 = 30;
+ var result= num1 - num2; //Subtraction
+ console.log(`Subtraction is ${result}`);
+
+ var result= num1 * num2; //Multiflication
+ console.log(`Multification is ${result}`);
+
+ var result= num1 / num2; //Dividation
+ console.log(`Dividation is ${result}`);
+
+ var result= num1 ** num2; //Exponential
+ console.log(`Exponential is ${result}`);
+
+ console.log(10/3);  // Divide operator
+ console.log(10%3);    //Modulus operator
+ console.log(11%3);  //Modulus operator
+ console.log(15%3);   //Modulus operator
+
+ var result =num1/3;
+ console.log(`Modulus/Remindar is ${result}`);
+
+ var num = 10 ;   //Compound addition +=
+ num+=20; // num = num + 20
+ console.log(`Compound addition += ${num}`);
+
+
+ var num = 10 ;   //Compound Subtraction -=
+ num-=20; // num = num - 20
+ console.log(`Compound Subtraction -= ${num}`);
+
+ var num =10;  //Compound Multification *=
+ num*=20;  // num =num*20
+ console.log(`Compound Multification *= ${num}`) ;
+
+
+ var num =10;  //Compound Dividation /=
+ num/=20;  // num =num/20
+ console.log(`Compound Dividation /= ${num}`) ;
+
+
+ console.log(`Comparison Operator`);
+
+ var num3 = 10;
+ console.log(num3==10);
+ console.log(num3==11);
+
+ var num3 = 10;
+ console.log(num3===10);
+ console.log(num3===11);
+
+
+ console.log(`Comparison Operator`);
+ var num3 = "10";
+ var num4 = 10;
+console.log(num3==num4);  //10==10
+ console.log(num3===num4);    //stricatly value
