3d886e89490475f8e9a7a390057a234d84cdeb3a
anitarupnawar <anitarupnawar@gmail.com>
2023-03-15 14:26:41 +0530

    05_NaN.js
    05_NaN.js

---
+++ 05_NaN.js
@@ -0,0 +1,25 @@
+
+console.log(10+"hi");
+
+console.log("Good" + "Morning");
+console.log("Good " + " Morning");
+
+console.log(10 -"hi");
+
+console.log("Operation : 10-hi" , 10- "h");
+
+console.log(0/0);
+console.log("Operation : 0/0" , 0/0);
+
+var num ="10";
+console.log(typeof num);
+
+var num2= +num;
+console.log(`${num} and it's type ${typeof num2}`);
+
+var myName= "Billgates";
+var result =+myName; 
+console.log(`${result} and it's type {type of result} `);
+
+console.log(10 + true);
+console.log(10 - true);
