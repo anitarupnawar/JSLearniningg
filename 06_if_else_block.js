373834e010d6daedc459856e94682d35eab33cc4
anitarupnawar <anitarupnawar@gmail.com>
2023-03-17 00:07:15 +0530

    06_if_else_block.js
    06_if_else_block.js

---
+++ 06_if_else_block.js
@@ -0,0 +1,17 @@
+
+function maleMarriageEligibility(gender, age, boyName) {
+    if(gender=="Male" && age>=21) {
+        var str = `Hey ${boyName} you are eligible for marriage`;
+        return str;
+
+    } else {
+        var str =` ${boyName} you are eligible for marriage` ;
+      return str;
+    }
+  
+      }
+      var result = maleMarriageEligibility("Male", 25, "Billgates");
+      console.log(result);
+      
+      var result = maleMarriageEligibility("Male", undefined, "Raju");
+      console.log(result);
