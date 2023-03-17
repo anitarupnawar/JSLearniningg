b3b6de30d465453baf877d21d41cc4c1474f0648
anitarupnawar <anitarupnawar@gmail.com>
2023-03-18 00:29:36 +0530

    I have updated for_reverse.js
    I have updated for_reverse.js

---
+++ 05_for_reverse_string.js
@@ -0,0 +1,23 @@
+// var reverseStr = function(str){
+// // var reverse ="";
+//     for (let index = str.length -1 ; index >=0; index--) {
+//         var charAt =str.charAt(index);
+//         reverse = reverse + charAt;
+//         }
+//         return reverse;
+// }
+// var result = reverseStr("Do it anyhow");
+// console.log(`Reverse last word : ${result}`);
+
+
+//I am Angular champ
+
+var str = "I am Angular champ"
+var count =0;
+for (let index =0 ; index < str.length; index++) {
+    var char =str. charAt(index);
+       if (char == " ") {
+        count = count +1;
+       }
+    }
+console.log ("Total number of space is :",count);
