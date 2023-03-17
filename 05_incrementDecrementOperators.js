796c1a9f3a9b5dc595beadbeec82189dcd1cac3f
anitarupnawar <anitarupnawar@gmail.com>
2023-03-17 22:15:44 +0530

    I have updated Increment & Decrement operator.js
    I have updated Increment & Decrement operator.js

---
+++ 05_incrementDecrementOperators.js
@@ -0,0 +1,25 @@
+//index++ = index=index+1
+//index-- = index= index -1
+
+
+//Example 1 -Post Increment
+
+var index =10;
+var y = index++
+console.log(index, y);
+
+//Example 2 -Post Decerement
+var index =10;
+var y = index--
+console.log(`index: ${index}, y: ${y}`);
+
+
+//Exmaple 3 - Pre Incerement
+var index =10;
+var y = ++index
+console.log(index, y);
+
+//Example 4 - Pre Decrement
+var index =10;
+var y = --index
+console.log(index, y);
