a3f7f2915015b23e4ff180166fd964c65658be20
anitarupnawar <anitarupnawar@gmail.com>
2023-03-17 21:46:36 +0530

    i have updated for_loop.js
    i have updated for_loop.js

---
+++ 06_for_loop.js
@@ -0,0 +1,50 @@
+
+// print number from 1 to 10
+//Initialization 1
+//update Expression + 1
+//condition <=10
+
+//Example 1
+
+for (let index = 1; index <=10; index= index+1) {
+    console.log(index);
+    }
+
+//Example 2
+//print 0 to 5
+for (let index = 0; index < 5; index = index+1) {
+    console.log(index);
+    
+}
+
+//Example 3
+// print 10 to 0
+
+for (let index = 10; index <= 0; index =index-1) {
+    console.log(index);
+    
+}
+
+//Example 4
+console.log("Table of 5");
+//Intilization condition update
+for (let index = 5; index <=50; index = index+5) {
+console.log(index);
+}
+
+
+//Example 5
+
+console.log("********************************");
+for (let index = 1; index <=10;) {
+    console.log(index);;
+    }
+
+    //Example 6
+
+for (let index = 10; index >=0; index++) { //index =10 
+    console.log(index);;
+    
+}
+//index++ means index = index+1
+
