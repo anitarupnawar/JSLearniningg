66ece37d54d0329da9c2b884db8de6f91e2faae9
anitarupnawar <anitarupnawar@gmail.com>
2023-07-24 23:43:04 +0530

    basicVariableAssignDemo.js
    basicVariableAssignDemo.js

---
+++ basicVariableAssignDemo.js
@@ -0,0 +1,23 @@
+var mobile= 9503884887;
+var age=28;
+var weight=55;
+var fname="Anita";
+var mname="Mukinda";
+var lname ="Rupnawar";
+var ismarried=false;
+var lazy=false;
+var isgraduate=true;
+console.log("mobile:",mobile,"type:",typeof mobile);
+console.log("age:", age, "type:",typeof age);
+console.log("weight:",weight,"type:",typeof weight);
+console.log("fname:",fname,"type:",typeof fname);
+console.log("mnamege:",mname,"type:",typeof mname);
+console.log("lname:",lname,"type:",typeof lname);
+
+console.log("isMarried:",ismarried,"type:",typeof ismarried);
+console.log("lazy:",lazy,"type:",typeof lazy);
+console.log("isgraduate:",isgraduate,"type:",typeof isgraduate);
+
+
+console.log("Undefined-When we declare variable and do not initlize any value that time varibale contain default value which is undefined");
+console.log("Null-bydefault of variable is null");
